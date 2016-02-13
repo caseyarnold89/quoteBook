7705ea8a2e105e6fd2a9d609d1fc4462ad90cd74
Casey Arnold <caseyarnold89@gmail.com>
2016-02-13 11:38:22 -0700

    Add and remove quotes work

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,6 +1,19 @@
 var app = angular.module("quoteBook");
 
-app.controller("mainCtrl", function($scope) {
-	    
+app.controller("mainCtrl", function($scope, dataService) {
+    var data;
+	$scope.quotes = dataService.getData();    
+    
+    $scope.addQuote = function(addInputQ, addInputA) {
+        dataService.addData(addInputQ, addInputA);
+        $scope.addInputQ = "";
+        $scope.addInputA = "";
+    };
+    
+    $scope.removeQuote = function(removeInput) {
+        dataService.removeData(removeInput);
+        $scope.removeInput = "";
+    };
+    // $scope.filterQuotes = ;
     
 });
